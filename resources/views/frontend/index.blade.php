e6d4c8782dd2b85e0f59d706908c1c9568fa7101
Vishal2827 <vishu28282828@example.com>
2025-06-18 11:44:50 +0400

    test2

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -87,7 +87,7 @@
                 </h1>
 
                 <p class="mb-8 leading-relaxed">
-                    In the following section we listed a number of screenshots of different parts of the project,
+                    Vishal parjapat,
                     Laravel Starter.
                 </p>
             </div>
